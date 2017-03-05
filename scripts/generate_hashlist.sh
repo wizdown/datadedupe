778ca8c44d1a03d625e6856c1f4dd8483ae02cbd
Abhishek Gupta <abhishekgupta.my@gmail.com>
2017-03-05 13:18:10 +0530

    generate_hashlist.sh if working

--- scripts/generate_hashlist.sh
+++ scripts/generate_hashlist.sh
@@ -1,13 +1,49 @@
-scripts_dir=home/abhishek/Desktop/major/scripts
-chunk_dir=home/abhishek/Desktop/major/chunk
-cur_dir=home/abhishek/Desktop/major
+script_dir=/home/abhishek/Desktop/major/scripts
+chunk_dir=/home/abhishek/Desktop/major/chunk
+cur_dir=/home/abhishek/Desktop/major
 
-if [! -d chunk ]
+if [ ! -d $chunk_dir ]
 then
   echo "ERROR: failed to locate directory chunk!"
   return 1
 fi
 
+if [ $# -ne 1 ]
+then
+  echo "ERROR: Invalid number of parameters!"
+  return 2
+fi
+
+inputfile=$1
+hash_list=${cur_dir}/${inputfile}.hashlist
+chunk_list=${cur_dir}/${inputfile}.chunklist
+
+if [ -f $hash_list ]
+then
+  rm $hash_list
+fi
+
+if [ -f $chunk_list ]
+then
+  rm $chunk_list
+fi
+
+
+ls $chunk_dir | sort -n > $chunk_list
+
+chunk_no=1
+no_of_chunks=`cat $chunk_list |  wc -l`
+
+
+while [ $chunk_no -le $no_of_chunks ]
+do
+  chunk_name=`sed -n "${chunk_no}p" $chunk_list`
+  chunk_name=${chunk_dir}/${chunk_name}
+
+  java -classpath $script_dir hash $chunk_name >> $hash_list
+  chunk_no=`expr $chunk_no + 1`
+
+done
 
 
 return 0
