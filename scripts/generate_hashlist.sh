dc533ccabb0abbd067478b0b4b422443dc29a3c5
Abhishek Gupta <abhishekgupta.my@gmail.com>
2017-03-05 12:57:03 +0530

    Generalised vars

--- scripts/generate_hashlist.sh
+++ scripts/generate_hashlist.sh
@@ -1,10 +1,13 @@
 scripts_dir=home/abhishek/Desktop/major/scripts
-chunk_dir
+chunk_dir=home/abhishek/Desktop/major/chunk
+cur_dir=home/abhishek/Desktop/major
 
 if [! -d chunk ]
 then
   echo "ERROR: failed to locate directory chunk!"
   return 1
 fi
+
+
 
 return 0
